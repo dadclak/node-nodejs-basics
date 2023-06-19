fc635853053b1c038eb4079545ece8ce4c0cd1f8
dadclak <kozanchyn.vladeo@gmail.com>
2023-06-19 23:06:38 +0200

    feature: implement function that prints all array of filenames from files folder into console

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,19 @@
+import fs from "fs";
+import {ERROR_MESSAGE} from "./consts/messages.js";
+import {getAbsolutePath} from "./utils.js";
+import {FILES_PATH} from "./consts/path.js";
+
+const FILE_PATH = getAbsolutePath(FILES_PATH);
+
 const list = async () => {
-    // Write your code here 
+    fs.exists(FILE_PATH, (exists) => {
+        if (exists) {
+            const list = fs.readdirSync(FILE_PATH);
+            console.table(list)
+        } else {
+            throw new Error(ERROR_MESSAGE);
+        }
+    });
 };
 
 await list();
