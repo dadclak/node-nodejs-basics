98be04a8a8964f8d4ed12b943c594add7cee020a
dadclak <kozanchyn.vladeo@gmail.com>
2023-06-19 23:00:38 +0200

    feature: implement function that deletes file fileToRemove.txt

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,18 @@
+import fs from "fs";
+import {ERROR_MESSAGE} from "./consts/messages.js";
+import {getAbsolutePath} from "./utils.js";
+import {FILES_PATH} from "./consts/path.js";
+
+const FILE_PATH = getAbsolutePath(`${FILES_PATH}/fileToRemove.txt`);
+
 const remove = async () => {
-    // Write your code here 
+    fs.exists(FILE_PATH, (exists) => {
+        if (exists) {
+            fs.unlinkSync(FILE_PATH);
+        } else {
+            throw new Error(ERROR_MESSAGE);
+        }
+    });
 };
 
 await remove();
